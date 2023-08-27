ad74b067ca90e6ddc3245c03325b5ee39a728495
DevopsTraining <srivastav.anuj@live.com>
2023-08-27 18:46:27 +0530

    added new code for pipeline

---
+++ Jenkinsfile/test-app-code.groovy
@@ -0,0 +1,16 @@
+pipeline {
+    agent {label "anuj-test-app"}
+    stages{
+        stage ("GIT Clone") {
+            steps {
+                checkout scm
+                echo 'The code has been cloned'
+            }
+        }
+        stage ("Deployment") {
+            steps {
+                sh "cp /var/lib/jenkins/workspace/${JOB_NAME}/index.html /var/www/html"
+            }
+        }
+    }
+}
