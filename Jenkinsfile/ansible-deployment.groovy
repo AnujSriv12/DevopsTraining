dfc512609bfd54cf08df81496bdf6f129146da26
Anuj Srivastava <43999271+AnujSriv12@users.noreply.github.com>
2023-10-15 22:22:37 +0530

    Update ansible-deployment.groovy

--- Jenkinsfile/ansible-deployment.groovy
+++ Jenkinsfile/ansible-deployment.groovy
@@ -3,7 +3,6 @@ pipeline {
     environment {
         inventorypath = "/var/lib/jenkins/workspace/${JOB_NAME}/inventory.ini"
         playbookpath  = "/var/lib/jenkins/workspace/${JOB_NAME}/deployment.yml"
-
     }
     stages {
         stage ('checkout') {
@@ -13,7 +12,7 @@ pipeline {
         }
         stage ('Deploy') {
             steps {
-                sudo -u ansible ansible-playbook -i  ${inventorypath} ${playbookpath} -vvv
+                sudo -u ansible ansible-playbook -i ${inventorypath} ${playbookpath} -vvv
             }
         }
     }
